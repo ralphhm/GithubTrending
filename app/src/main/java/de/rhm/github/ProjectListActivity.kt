f77e45d3e615eaa68678e9d7104f9e916ba9e755
ralphhm <ralphhm+github@gmail.com>
2018-04-26 09:25:58 +0200

    Initial commit - Setup blank single activity project

---
+++ app/src/main/java/de/rhm/github/ProjectListActivity.kt
@@ -0,0 +1,14 @@
+package de.rhm.github
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_project_list.*
+
+class ProjectListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_list)
+        setSupportActionBar(toolbar)
+    }
+}
